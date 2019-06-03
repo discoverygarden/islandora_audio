f68b2993bf6fe78d7934e3b1a297c470f7aa1757
JojoVes <JojoVes@users.noreply.github.com>
2019-06-03 14:47:30 -0300

    Description tweaks.

--- src/Form/Admin.php
+++ src/Form/Admin.php
@@ -65,13 +65,13 @@ class Admin extends ConfigFormBase {
         '#type' => 'checkbox',
         '#title' => $this->t('Defer audio derivative generation during ingest'),
         '#description' => $this->t('Prevent audio derivatives from running during ingest,
-              useful if derivatives are to be created by an external service.'),
+          useful if derivatives are to be created by an external service.'),
         '#default_value' => $config->get('islandora_audio_defer_derivatives_on_ingest'),
       ],
       'islandora_lame_url' => [
         '#type' => 'textfield',
         '#title' => $this->t("Path to LAME"),
-        '#description' => $this->t('@LAME is required to create derivatives files.<br/>',
+        '#description' => $this->t('@LAME is required to create derivative files.<br/>',
                         [
                           '@LAME' => Link::fromTextAndUrl($this->t('LAME'), Url::fromUri('http://lame.sourceforge.net/'))->toString(),
                         ]) . islandora_executable_available_message($lame),
@@ -92,14 +92,16 @@ class Admin extends ConfigFormBase {
         '#max' => 9.999,
         '#step' => 0.001,
         '#title' => $this->t('MP3 derivative quality'),
-        '#description' => $this->t('Variable Bit Rate quality setting (0=highest quality, 9.999=lowest). Default = 5.'),
+        '#description' => $this->t('Variable Bit Rate quality setting
+          (0 = highest quality, 9.999 = lowest). Default = 5.'),
         '#size' => 5,
         '#default_value' => $config->get('islandora_audio_vbr_quality'),
       ],
       'islandora_audio_obj_fallback' => [
         '#type' => 'checkbox',
         '#title' => $this->t('Use original file as fallback'),
-        '#description' => $this->t('Attempt to play the OBJ datastream in the player if the PROXY_MP3 derivative is not present.'),
+        '#description' => $this->t('Attempt to play the OBJ datastream in the
+          player if the PROXY_MP3 derivative is not present.'),
         '#default_value' => $config->get('islandora_audio_obj_fallback'),
       ],
     ];
